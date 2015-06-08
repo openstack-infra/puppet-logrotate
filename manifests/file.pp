ad98fc9d501ab4d4bbe439fc4c93c6c459eaf8bd
Spencer Krum <nibz@spencerkrum.com>
2015-06-08 10:32:07 -0700

    Make logrotate::file more resilient
    Validate that the first option is a string.
    Mutate the path to remove /'s, allowing full paths to be passed.
    
    Prior to this, it looks like we are passing basenames to logrotate and
    the logrotate daemon is just nice enough to add /var/log/ to the
    beginning of the file to be rotated. I don't see this documented
    anywhere in the logrotate docs.
    
    Change-Id: I341290c430023e5e9a24bf2247482ed818ea4a26

--- manifests/file.pp
+++ manifests/file.pp
@@ -10,15 +10,25 @@ define logrotate::file( $log,
 
   # $options should be an array containing 1 or more logrotate
   # directives (e.g. missingok, compress).
+  validate_string($options[0])
 
   include logrotate
+  # This allows us to handle fully pathed files
+  $escaped_path = regsubst($name, '/', '_', 'G')
 
-  file { "/etc/logrotate.d/${name}":
+  file { "/etc/logrotate.d/${escaped_path}":
     ensure  => $ensure,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('logrotate/config.erb'),
     require => File['/etc/logrotate.d'],
+  }
+
+  # Ensure logrotate rules managed by old versions of this module
+  # are purged
+  $file_basename = basename($name)
+  file { "/etc/logrotate.d/${file_basename}":
+    ensure => absent,
   }
 }
